c2ece7e94d75a81b38cb256e23a55eafd5acf6f1
Marcus Reis <mvsantosreis@gmail.com>
2024-05-08 13:30:28 +0100

    Components, Props and Emitters - Part1

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <listItem name="John" department="IT" salary="99" position="1" />
+  </main>
+</template>
+
+<script setup>
+import listItem from "./components/listItem.vue";
+import { ref } from "vue";
+
+let database = ref([
+  {
+    name: "Johnny",
+    department: "Marketing",
+    salary: 30000,
+  },
+  {
+    name: "Sarah",
+    department: "Engineering",
+    salary: 72000,
+  },
+  {
+    name: "Terrance",
+    department: "Executive",
+    salary: 125000,
+  },
+]);
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/listItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h2>Name: {{ name }}</h2>
+    <p>Department: {{ department }}</p>
+    <p>Salary: {{ salary }}</p>
+    <p>Position: {{ position }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  department: String,
+  salary: Number,
+  position: Number,
+});
+</script>
+
+<style scoped>
+h2 {
+  padding: 0;
+  margin: 0;
+}
+.container {
+  border: 1px solid rgb(124, 191, 202);
+  width: 300px;
+  margin: 1em auto 1em auto;
+  padding: 1em;
+  border-radius: 5px;
+  font-size: 1.3em;
+}
+</style>
