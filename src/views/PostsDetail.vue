5d40be92880f5fc5df38aa1dcd8f4526f41b6c27
Marcus Reis <mvsantosreis@gmail.com>
2024-05-09 13:07:42 +0100

    Router - Basic Options

--- src/views/PostsDetail.vue
+++ src/views/PostsDetail.vue
@@ -8,5 +8,34 @@
     {{ this.$route.params.id }}
   </h1>
   <p><router-link to="/posts">Back</router-link></p>
+  <button @click="showPostID">Show Post ID</button>
+  <br />
+  <button @click="goHomeIn3Secs(3000)">Go Home in 3 secs</button>
+  <br />
+  <button @click="goToPost('id1')">Go to post ID</button>
 </template>
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+import { useRouter } from "vue-router";
+const route = useRoute();
+const router = useRouter();
+
+const showPostID = () => {
+  console.log(`The ID of this post is: ${route.params.id}`);
+};
+
+const goHomeIn3Secs = (seconds) => {
+  setTimeout(() => {
+    router.push("/");
+    // router.push({ name: "Emitters" });
+  }, seconds);
+};
+const goToPost = (id) => {
+  router.push({
+    name: "PostsDetail",
+    params: {
+      id: "id1",
+    },
+  });
+};
+</script>
