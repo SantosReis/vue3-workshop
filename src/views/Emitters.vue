5c458bc8fc05a812af48e570504ff1bd3aa50952
Marcus Reis <mvsantosreis@gmail.com>
2024-05-09 11:44:04 +0100

    Restore Emitters sample

--- src/views/Emitters.vue
+++ src/views/Emitters.vue
@@ -1,3 +1,54 @@
 <template>
-  <h1>Another One</h1>
+  <main>
+    <ChildComponent @add="(i) => (count += i)" />
+    <p>Count: {{ count }}</p>
+
+    <listItem
+      v-for="(item, index) in database"
+      :name="item.name"
+      :department="item.department"
+      :salary="item.salary"
+      :position="index"
+      v-on:deleteItem="deleteItem"
+      :key="index"
+    />
+  </main>
 </template>
+
+<script setup>
+import listItem from "../components/ListItem.vue";
+import ChildComponent from "../components/Child.vue";
+import { ref } from "vue";
+
+let database = ref([
+  {
+    name: "Johnny",
+    department: "Marketing",
+    salary: 30000,
+  },
+  {
+    name: "Sarah",
+    department: "Engineering",
+    salary: 72000,
+  },
+  {
+    name: "Terrance",
+    department: "Executive",
+    salary: 125000,
+  },
+]);
+
+const count = ref(0);
+
+function deleteItem(position) {
+  database.value.splice(position, 1);
+}
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
